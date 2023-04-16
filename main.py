9f31f3d5dc3b3583196d24eba6ca449128d3956c
theguyben <64026118+theguyben@users.noreply.github.com>
2023-04-16 17:32:05 +0000

    Adding docstrings

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+"""
+This code bla bla bla...
+"""
 import numpy as np  # comment
 
 
 def print_hi(name):
+    """
+    Prints a greeting and arrays using NumPy.
+    Args:
+        name (str): The name to include in the greeting.
+    Returns:
+        None: This function does not return any value.
+    Raises:
+        N/A
+    Example:
+        >>> print_hi("Alice")
+        [1 2 3]
+        Bye, Alice
+        [4 5 6]
+        [7 8 9]
+        [10 11 12]
+        [1 2 3] [4 5 6] [7 8 9] [10 11 12]
+    """
     first = np.array([1, 2, 3])
     print(first)
     print(f'Bye, {name}')
