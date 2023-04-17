cde81db66ed8e9c536062ae5967f85a2a22a28ab
noamiluztech <118074647+noamiluztech@users.noreply.github.com>
2023-04-17 14:57:37 +0000

    bug fix for save_camera_position

--- calibration/save_camera_position.py
+++ calibration/save_camera_position.py
@@ -1,19 +1,24 @@
-# DroNet-2023
-# This function will save calibration values of a specific camera
+""""
+ DroNet-2023
+"""
 
-import json, os
+import json
+import os
 
 def save_values(parameters) -> None:
+    """
+    function that gets parameters type .... 
+    saving file in json type for each camera.
+    """
     output_directory = 'data'
-   
+
     # Create the directory if it doesn't exist
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    # Set the path 
+    # Set the path
     output_file_path = os.path.join(output_directory, 'parameters.json')
 
     # 'w' for write mode
-    with open(output_file_path, 'w', encoding='utf-8') as f:        
-        json.dump(parameters, f)
-
+    with open(output_file_path, 'w', encoding='utf-8') as form:
+        json.dump(parameters, form)
