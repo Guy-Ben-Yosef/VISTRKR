0901cdf0ba7c9f4f14a2dc8a48cfa9e0d2348adb
theguyben <guyben@campus.technion.ac.il>
2023-05-04 14:45:17 +0300

    Defeated pycharm warnings

--- calibration/calib_functions.py
+++ calibration/calib_functions.py
@@ -4,15 +4,14 @@ import numpy as np
 
 
 def calculate_calibration_params(measured_pixels, expected_angles, fit_degree):
-    coef, residuals, _, _, _, = np.polyfit(measured_pixels, expected_angles, deg=fit_degree, full=True)
-    slope = coef[0]
-    intercept = coef[1]
+    coefficients, residuals, _, _, _, = np.polyfit(measured_pixels, expected_angles, deg=fit_degree, full=True)
+    slope = coefficients[0]
+    intercept = coefficients[1]
     residuals = residuals[0]
 
     # Compute R^2
     total_sum_of_squares = sum((expected_angles - np.mean(expected_angles)) ** 2)
     r_squared = 1 - (residuals / total_sum_of_squares)
-
 
     if r_squared < 0.95:
         warnings.warn(f"Too small residual (R^2 = {r_squared:0.3f})")
@@ -24,14 +23,10 @@ def calculate_calibration_params(measured_pixels, expected_angles, fit_degree):
 def calculate_expected_angles(camera_position, points, camera_azimuth):
     expected_angles = []
     for point in points:
-        x = point[0];
-        y = point[1];
+        x = point[0]
+        y = point[1]
         delta_x = x - camera_position[0]  # Calculate the difference in x coordinates
         delta_y = y - camera_position[1]  # Calculate the difference in y coordinates
         azimuth = np.rad2deg(np.arctan2(delta_y, delta_x)) - camera_azimuth  # Calculate the azimuth angle
         expected_angles.append(azimuth)
     return expected_angles
-
-
-def foo(a, b):
-    return a + b
